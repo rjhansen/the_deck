a19c8d5db8100011077a23472d7d1c0ae5cb4dae
Eugene Libster <eugenecollege@yahoo.com>
2024-12-17 09:04:09 -0500

    source addition

---
+++ decky/decky.h
@@ -0,0 +1,26 @@
+enum class Suit
+{
+    HEART = 0,
+    DIAMOND,
+    CLUB,
+    SPADE,
+    NONE
+};
+
+enum class Rank
+{
+    TWO = 0,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE,
+    JOKER
+}
