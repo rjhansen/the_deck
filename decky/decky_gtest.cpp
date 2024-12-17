ced93f26d9bf9a935fefd436f67ab2c3b8a11b24
Eugene Libster <eugenecollege@yahoo.com>
2024-12-17 08:50:28 -0500

    moar source

---
+++ decky/decky_gtest.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "decky.h"
+
+
+// Test two things: deck and cards
+// card: suit, rank
+// card: less_than, equal
+
+// deck: container of cards
+// deck: test random access into deck
+// deck: initially sorted
+// deck: shuffle
+// deck: sort
+// deck: deal from arbitrary position (top, middle, bottom)
+// deck: insert card into arbitrary position (top, middle, bottom)
+
+// STRETCH GOAL: Implement Solitaire (crypto cipher)
