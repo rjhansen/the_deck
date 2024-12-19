7f7c28c8e2a3b983b4a372ccd1039c7620bdde0e
Eugene Libster <eugenecollege@yahoo.com>
2024-12-19 13:33:07 -0500

    fixed parens

--- decky/card.cpp
+++ decky/card.cpp
@@ -44,7 +44,7 @@ int32_t Card::card_as_int() const
     if(SUIT==Suit::NONE && RANK==Rank::JOKER_A)
         return 52; // For the Solitaire algorithm
 
-    if(SUIT==Suit::NONE && RANK == Rank::JOKER_B))
+    if(SUIT==Suit::NONE && RANK == Rank::JOKER_B)
         return 53; // For the Solitaire algorithm
 
     return static_cast<int32_t>(SUIT)*13 + static_cast<int32_t>(RANK);
