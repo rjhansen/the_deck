7ad5885992ecc0be8df7d8338099701685ddb01d
Rob Hansen <rob@hansen.engineering>
2025-01-24 12:20:04 -0500

    Fixed the Windows build script.

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
 #!/usr/bin/env pwsh
 
 $version = "1.0.0"
-$win_output = the_deck-win64-$version
+$win_output = "the_deck-win64-$version"
 
 If ($IsWindows) {
-    Remove-Item -Recurse -Force ..\the_deck-win64-$version
-    New-Item -Name $win_output -ItemType Directory
-    New-Item -Name $win_output\dev -ItemType Directory
-    New-Item -Name $win_output\test_suite -ItemType Directory
+    Remove-Item -Recurse -Force "$win_output"
+    New-Item -Name "$win_output" -ItemType Directory
+    New-Item -Name "$win_output\dev" -ItemType Directory
+    New-Item -Name "$win_output\test_suite" -ItemType Directory
 }
 
 Remove-Item -Recurse -Force build
@@ -17,13 +17,13 @@ Set-Location -Path build
 If ($IsWindows) {
     cmake -S .. -B . 
     msbuild the_deck.sln -p:Configuration=Release
-    Copy-Item decky\Release\* -Destination ..\$win_output\dev
-    Copy-Item ..\decky\the_deck.h -Destination ..\$win_output\dev
-    Copy-Item bin\Release\gtest.dll -Destination ..\$win_output\test_suite
-    Copy-Item bin\Release\gtest_main.dll -Destination ..\$win_output\test_suite
-    Copy-Item tests\Release\decky_gtest.exe -Destination ..\$win_output\test_suite
+    Copy-Item decky\Release\* -Destination "..\$win_output\dev"
+    Copy-Item ..\decky\the_deck.h -Destination "..\$win_output\dev"
+    Copy-Item bin\Release\gtest.dll -Destination "..\$win_output\test_suite"
+    Copy-Item bin\Release\gtest_main.dll -Destination "..\$win_output\test_suite"
+    Copy-Item tests\Release\decky_gtest.exe -Destination "..\$win_output\test_suite"
     Set-Location -Path ..
-    Compress-Archive -Path=$win_output -DestinationPath=$win_output.zip
+    Compress-Archive -Path "$win_output" -DestinationPath "$win_output.zip"
 }
 Else { 
     cmake -S .. -B . -G Ninja -D CMAKE_BUILD_TYPE=Release
